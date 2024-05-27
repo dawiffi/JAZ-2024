3bae39ea309b51d064817f62a1469693830b2998
s29608-pj <dawid.frontczak11@gmail.com>
2024-05-27 15:53:03 +0200

    Delete zjazd3/dfrontczak directory

--- zjazd3/dfrontczak/src/main/java/com/example/dfrontczak/ChampionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.dfrontczak;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChampionRepository extends JpaRepository<Champion, Long> {
-}
--- zjazd3/dfrontczak/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO champion (id, damage_type, name, region, role, type) VALUES (1, 'AP', 'Teemo', 'Bandle City', 'Top', 'Ranged')
-INSERT INTO champion (id, damage_type, name, region, role, type) VALUES (2, 'AD', 'Master Yi', 'Ionia', 'Jungle', 'Melee')
-INSERT INTO champion (id, damage_type, name, region, role, type) VALUES (3, 'AD', 'Garen', 'Demacia', 'Top', 'Melee')
