c7d2b399d839c3d7914e2085fc589f8f94b01aa1
s29608-pj <dawid.frontczak11@gmail.com>
2024-06-10 14:18:33 +0200

    Delete src directory

--- src/main/java/com/example/champions_app/ChampionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.champions_app;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChampionRepository extends JpaRepository<Champion, Long> {
-}
